143b1415a6d8323e3b0cc5622728754ad33b3ddc
Anant Agnihotri <anantrevived@gmail.com>
2023-09-05 18:49:59 +0530

    initial commit

---
+++ dist/precache-manifest.cdd35ccf76d636dbf2d5e679ce5aca45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec721464a803e7e8df2f2167c6bb41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8563155c7454d5d2ac74",
+    "url": "/static/css/main.ef09b238.chunk.css"
+  },
+  {
+    "revision": "290cc22a92d5ff616f73",
+    "url": "/static/js/2.b5465df7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b5465df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8563155c7454d5d2ac74",
+    "url": "/static/js/main.2bb7bdd6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2bb7bdd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d34c1ef83c210c405ce",
+    "url": "/static/js/runtime-main.e9946a1e.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/static/media/city.e1698332.jpg"
+  }
+]);
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6043c565b351c0f25e247dd79b4d4d19",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
